3a8a5f81961958fb65839b4b99513a14413918c2
esophagoose <118142981+esophagoose@users.noreply.github.com>
2023-12-27 20:00:08 -0800

    Added writing gerbers

--- layers/aperture.py
+++ layers/aperture.py
@@ -1,11 +1,11 @@
-import typing
 import enum
-import re
 import logging
 import math
+import re
+from typing import Any, Dict, List, NamedTuple, Tuple
 
 
-class ApertureCircle(typing.NamedTuple):
+class ApertureCircle(NamedTuple):
     diameter: float
     cx: float = 0
     cy: float = 0
@@ -15,7 +15,7 @@ class ApertureCircle(typing.NamedTuple):
         return self.diameter / 2
 
 
-class ApertureRectangle(typing.NamedTuple):
+class ApertureRectangle(NamedTuple):
     width: float
     height: float
     cx: float = 0
@@ -23,14 +23,14 @@ class ApertureRectangle(typing.NamedTuple):
     rotation: float = 0
 
 
-class ApertureObround(typing.NamedTuple):
+class ApertureObround(NamedTuple):
     width: float
     height: float
     cx: float = 0
     cy: float = 0
 
 
-class AperturePolygon(typing.NamedTuple):
+class AperturePolygon(NamedTuple):
     diameter: float
     vertices: int
     rotation: float = 0
@@ -38,8 +38,8 @@ class AperturePolygon(typing.NamedTuple):
     cy: float = 0
 
 
-class ApertureOutline(typing.NamedTuple):
-    points: typing.Tuple[int]
+class ApertureOutline(NamedTuple):
+    points: Tuple[int]
     rotation: float = 0
 
 
@@ -54,16 +54,18 @@ class MacroPrimitive(enum.Enum):
     THERMAL = 7
 
 
-class Aperture(typing.NamedTuple):
+class Aperture(NamedTuple):
     index: str
     exposure: bool
-    shape: typing.Any
+    shape: Any
     rotation: float = 0
     hole: float = 0
+    comments: List[str] = []
 
 
-class Macro(typing.NamedTuple):
-    statements: typing.List[tuple[MacroPrimitive, str]]
+class Macro(NamedTuple):
+    name: str
+    statements: List[Tuple[MacroPrimitive, str]]
 
     def validate_values(self, values):
         data = [text for _, text in self.statements]
@@ -72,7 +74,7 @@ class Macro(typing.NamedTuple):
         error = f"Invalid values! Got {len(values)}, expected {count}"
         assert count == len(values), error
 
-    def _parse(self, values: typing.List[float]):
+    def _parse(self, values: List[float]):
         parsed = []
         for p, statement in self.statements:
             for i, value in enumerate(values):
@@ -80,14 +82,15 @@ class Macro(typing.NamedTuple):
             parsed.append((p, [float(v) for v in statement.split(",")]))
         return parsed
 
-    def generate_aperture(self, index: int, values: typing.List[float]):
+    def generate_aperture(self, index: int, values: List[float], comments):
         self.validate_values(values)
-        shape = None
-        exposure = True
-        rotation = 0
+        shapes = []
         for primitive, statement in self._parse(values):
+            shape = None
+            exposure = True
+            rotation = 0
             if primitive == MacroPrimitive.CIRCLE:
-                exposure, diameter, cx, cy, _ = statement
+                exposure, diameter, cx, cy, rotation = statement
                 shape = ApertureCircle(
                     diameter=diameter,
                     cx=cx,
@@ -116,8 +119,11 @@ class Macro(typing.NamedTuple):
                 exposure = bool(statement[0])
                 verticies = statement[1] + 1  # initial point isn't counted
                 rotation = statement[-1]
-                points = statement[2:-1]
-                assert len(points) == 2 * verticies, "Malformed command"
+                points = [
+                    (statement[i], statement[i + 1])
+                    for i in range(2, len(statement) - 1, 2)
+                ]
+                assert len(points) == verticies, "Malformed command"
                 shape = ApertureOutline(points=points, rotation=rotation)
             elif primitive == MacroPrimitive.POLYGON:
                 exposure, verticies, cx, cy, d, rotation = statement
@@ -135,17 +141,58 @@ class Macro(typing.NamedTuple):
                 raise NotImplementedError(MacroPrimitive.THERMAL)
             else:
                 raise NotImplementedError(statement)
-        return Aperture(
-            index=index, exposure=exposure, shape=shape, rotation=rotation, hole=0
-        )
+            shapes.append(shape)
+        return Aperture(index, exposure, shapes, rotation, 0, comments)
+
+    def from_aperture(self, aperture: Aperture):
+        for i, shape in enumerate(aperture.shape):
+            primitive, statement = self.statements[i]
+            variables = set(re.findall(r"\$\d+", statement))
+            variable_indicies = []
+            for variable in sorted(variables):
+                variable_indicies.append(statement.split(",").index(variable))
+            if primitive == MacroPrimitive.CIRCLE:
+                values = [
+                    aperture.exposure,
+                    shape.diameter,
+                    shape.cx,
+                    shape.cy,
+                    aperture.rotation,
+                ]
+            elif primitive == MacroPrimitive.VECTOR_LINE:
+                raise NotImplementedError(MacroPrimitive.VECTOR_LINE)
+            elif primitive == MacroPrimitive.CENTER_LINE:
+                raise NotImplementedError(MacroPrimitive.CENTER_LINE)
+            elif primitive == MacroPrimitive.OUTLINE:
+                values = [aperture.exposure, len(shape.points) - 1]
+                values.extend([x for p in shape.points for x in p])
+                values.append(aperture.rotation)
+            elif primitive == MacroPrimitive.POLYGON:
+                values = [
+                    aperture.exposure,
+                    shape.vertices,
+                    shape.cx,
+                    shape.cy,
+                    shape.diameter,
+                    aperture.rotation,
+                ]
+            elif primitive == MacroPrimitive.MOIRE:
+                raise NotImplementedError(MacroPrimitive.MOIRE)
+            elif primitive == MacroPrimitive.THERMAL:
+                raise NotImplementedError(MacroPrimitive.THERMAL)
+            else:
+                raise NotImplementedError(statement)
+            parsed = "X".join([str(values[i]) for i in variable_indicies])
+            return f"{self.name},{parsed}"
 
 
 class ApertureFactory:
     def __init__(self):
-        self.macros: typing.Dict[str, Macro] = {}
+        self.macros: Dict[str, Macro] = {}
+        self._macro_map: Dict[str, int] = {}
 
-    def from_aperture_define(self, statement):
-        def pad_optional_params(params: typing.List[float], count: int):
+    def from_aperture_define(self, statement, comments=[]):
+        def pad_optional_params(params: List[float], count: int):
             return params + [0] * (count - len(params))
 
         pattern = re.compile(r"^D(\d+)([A-z]+),([\d.X]+)$")
@@ -154,7 +201,8 @@ class ApertureFactory:
         hole = 0
         if shape in self.macros:
             macro = self.macros[shape]
-            return macro.generate_aperture(int(aperture_id), parameters)
+            self._macro_map[int(aperture_id)] = shape
+            return macro.generate_aperture(int(aperture_id), parameters, comments)
         elif shape == "C":
             diameter, hole = pad_optional_params(parameters, 2)
             shape = ApertureCircle(diameter=diameter)
@@ -170,8 +218,29 @@ class ApertureFactory:
         else:
             raise ValueError(f"Invalid aperture shape: {statement}")
         return Aperture(
-            index=int(aperture_id), exposure=True, shape=shape, rotation=0, hole=hole
+            index=int(aperture_id),
+            exposure=True,
+            shape=shape,
+            rotation=0,
+            hole=hole,
+            comments=comments,
         )
+
+    def to_aperture_define(self, aperture: Aperture) -> str:
+        if isinstance(aperture.shape, list):
+            # Handling macros
+            shape = self._macro_map[int(aperture.index)]
+            macro = self.macros[shape]
+            define = macro.from_aperture(aperture)
+            return f"ADD{aperture.index}{define}"
+        else:
+            shape = type(aperture.shape).__name__.replace("Aperture", "")[0]
+            params = "X".join(
+                str(p)
+                for p in aperture.shape._asdict().values()
+                if isinstance(p, float) and p != 0
+            )
+            return f"ADD{aperture.index}{shape},{params}"
 
     def define_macro(self, statement):
         data = statement.split("*\n")
@@ -187,4 +256,11 @@ class ApertureFactory:
                 continue
             assert row[1] == ",", "Malformed macro"
             statements.append((primitive, row[2:]))
-        self.macros[name] = Macro(statements)
+        self.macros[name] = Macro(name, statements)
+
+    def macro_to_str(self, macro: Macro) -> str:
+        statement = f"%AM{macro.name}*\n"
+        for primitive, params in macro.statements:
+            statement += f"{primitive.value},{params}*\n"
+        statement += "%"
+        return statement
--- layers/gerber_layer.py
+++ layers/gerber_layer.py
@@ -4,9 +4,10 @@ import logging
 import os
 import re
 import typing
+from typing import List, Tuple
 
-import standard.gerber as gf
 import layers.aperture as aperture_lib
+import standard.gerber as gf
 
 
 class Units(enum.Enum):
@@ -15,19 +16,13 @@ class Units(enum.Enum):
     UNKNOWN = "XX"
 
 
-class QuadrantMode(enum.Enum):
-    SINGLE = 1
-    MULTI = 2
-    UNKNOWN = 3
-
-
 class OperationState(typing.NamedTuple):
     aperture: aperture_lib.Aperture
     interpolation: gf.GerberFormat
     point: tuple
     previous_point: tuple
     polarity: bool
-    quadrant_mode: QuadrantMode
+    quadrant_mode: gf.GerberFormat
     scalars: tuple
     units: Units
 
@@ -45,6 +40,8 @@ class GerberLayer:
         logging.info(f"\tFile: {filepath}")
         logging.info(f"\tType: {self.file_type.upper()}")
 
+        self._in_header = True
+        self.header = []
         self.current_aperture = None
         self.interpolation = None
         self.attributes = {}
@@ -53,13 +50,13 @@ class GerberLayer:
         self.path = filepath
         self.apertures = {}
         self.current_point = None
-        self.comments = ""
+        self.comments = []
         self.units = Units.UNKNOWN
-        self.quadrant_mode = QuadrantMode.UNKNOWN
+        self.quadrant_mode = None
         self.scalars = ()
-        self.sigfig_x = 1
-        self.sigfig_y = 1
-        self.operations = []
+        self.decimal_digits = (0, 0)
+        self.integer_digits = (0, 0)
+        self.operations: List[Tuple[gf.GerberFormat, OperationState]] = []
         self._regions = []
         self.aperture_factory = aperture_lib.ApertureFactory()
         self.collection_of_region = []
@@ -98,8 +95,12 @@ class GerberLayer:
             if content:  # this is rare and poor syntax
                 self._process(content, raise_on_unknown_command)
         elif op_type == gf.GerberFormat.COMMENT:
-            self.comments += content + "\n"
+            if self._in_header:
+                self.header.append(content)
+            else:
+                self.comments.append(content)
         elif op_type == gf.GerberFormat.UNITS:
+            self._in_header = False
             self.units = Units(data[2:])
             logging.info(f"Switching units to {self.units}")
         elif op_type in [
@@ -109,12 +110,12 @@ class GerberLayer:
             u = "MM" if op_type == gf.GerberFormat.DEPRECATED_UNITS_MM else "INCH"
             self.units = Units(u)
             logging.info(f"Switching units to {self.units}")
-        elif op_type == gf.GerberFormat.QUADMODE_SINGLE:
-            logging.info("Switching to single quadrant mode")
-            self.quadrant_mode = QuadrantMode.SINGLE
-        elif op_type == gf.GerberFormat.QUADMODE_MULTI:
-            logging.info("Switching to multi quadrant mode")
-            self.quadrant_mode = QuadrantMode.MULTI
+        elif op_type in [
+            gf.GerberFormat.QUADMODE_SINGLE,
+            gf.GerberFormat.QUADMODE_MULTI,
+        ]:
+            logging.info(f"Switching quadrant mode to: {op_type}")
+            self.quadrant_mode = op_type
         elif op_type == gf.GerberFormat.FORMAT:
             self._set_scalars(data)
             logging.info(f"Got decimal places: {self.scalars}")
@@ -122,9 +123,11 @@ class GerberLayer:
             self.polarity = content == "D"
             logging.info(f"Setting polarity to {self.polarity}")
         elif op_type == gf.GerberFormat.APERTURE_DEFINE:
-            aperture = self.aperture_factory.from_aperture_define(content)
-            print(aperture.index, type(aperture.index))
+            aperture = self.aperture_factory.from_aperture_define(
+                content, copy.deepcopy(self.comments)
+            )
             self.apertures[aperture.index] = aperture
+            self.comments.clear()
             logging.info(f"Add aperture: {aperture.index}")
         elif op_type == gf.GerberFormat.APERTURE_MACRO:
             self.aperture_factory.define_macro(content)
@@ -171,9 +174,66 @@ class GerberLayer:
             if raise_on_unknown_command:
                 raise ValueError(f"Unknown command: {data}")
 
+    def point_to_text(self, point):
+        assert point[0] < pow(10, self.integer_digits.x), "Overflow x value"
+        assert point[1] < pow(10, self.integer_digits.y), "Overflow y value"
+        x = int(point[0] * pow(10, self.decimal_digits.x))
+        y = int(point[1] * pow(10, self.decimal_digits.y))
+        return f"X{x}Y{y}"
+
+    def write(self, filename):
+        def write_line(message: str, _file, grouped=False):
+            message += "*"
+            if grouped:
+                message = f"%{message}%"
+            _file.write(message + "\n")
+
+        state = self.operations[0][1]
+        with open(filename, "w") as f:
+            current_aperture = None
+            for comment in self.header:
+                write_line(gf.GerberFormat.COMMENT.value + comment, f)
+            write_line(gf.GerberFormat.UNITS.value + state.units.value, f, True)
+            format_spec = gf.Point(
+                x=(self.integer_digits.x * 10 + self.decimal_digits.x),
+                y=(self.integer_digits.y * 10 + self.decimal_digits.y),
+            )
+
+            write_line("FSLA" + format_spec.to_text(), f, True)
+            write_line(state.quadrant_mode.value, f)
+            for macro in self.aperture_factory.macros.values():
+                write_line(self.aperture_factory.macro_to_str(macro), f)
+            for aperture in self.apertures.values():
+                for comment in aperture.comments:
+                    write_line(gf.GerberFormat.COMMENT.value + comment, f)
+                statement = self.aperture_factory.to_aperture_define(aperture)
+                write_line(statement, f, True)
+            # import code
+
+            # code.interact(local=locals())
+            polarity = gf.GerberFormat.LOAD_POLARITY.value
+            polarity += "D" if state.polarity else "C"
+            write_line(polarity, f, True)
+            # return OperationState(
+            #     aperture=aperture if not self.region else None,
+            #     polarity=self.polarity,
+            #     units=self.units,
+            #     quadrant_mode=self.quadrant_mode,
+            #     scalars=self.scalars,
+            #     interpolation=self.interpolation,
+            #     previous_point=self.current_point,
+            #     point=point,
+            # )
+            for op_type, op in self.operations:
+                if op.aperture and op.aperture != current_aperture:
+                    write_line(f"D{op.aperture.index}", f)
+                    current_aperture = op.aperture
+                write_line(self.point_to_text(op.point) + op_type.value, f)
+            write_line(gf.GerberFormat.END_OF_FILE.value, f)
+
     def scale(self, point):
-        x = round(point[0] * self.scalars[0], self.sigfig_x)
-        y = round(point[1] * self.scalars[1], self.sigfig_y)
+        x = round(point[0] * self.scalars[0], self.decimal_digits.x)
+        y = round(point[1] * self.scalars[1], self.decimal_digits.y)
         return x, y
 
     def _run_operation(self, content: str):
@@ -202,7 +262,7 @@ class GerberLayer:
         match = re.search(regex, text)
         if not match:
             raise RuntimeError("No decimal places available!")
-        _, decx, _, decy = match.groups()
+        intx, decx, inty, decy = match.groups()
         self.scalars = (pow(10, -int(decx)), pow(10, -int(decy)))
-        self.sigfig_x = int(decx)
-        self.sigfig_y = int(decy)
+        self.integer_digits = gf.Point(int(intx), int(inty))
+        self.decimal_digits = gf.Point(int(decx), int(decy))
